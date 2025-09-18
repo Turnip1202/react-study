63023b921d87bbab786fffdda4eedd9b859c25b0
Turnip1202 <turnip1202@outlook.com>
2025-09-18 14:46:11 +0800

    change

--- src/pages/PerformanceOptimization.tsx
+++ src/pages/PerformanceOptimization.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback, memo, useRef, useEffect } from 'react'
+import React, { useState, useMemo, useCallback, memo, useRef } from 'react'
 import './PerformanceOptimization.css'
 
 // Expensive calculation simulation
